01851914205b20a2fe2b8f558000a6aa13d65abf
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-06-23 23:41:03 +0530

    fix backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(express.json())
 app.use(cors({
   origin: '*',
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+ credentials: true
 }));
 app.use('/api/results', resultRoutes); 
 
